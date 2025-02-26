60159d59a6146fa6b67e3b03d4922aa3654b8d1e
Sattia12 <sattia1@hotmail.com>
2025-02-26 15:40:23 +0000

    add search to dropdown

--- frontend/js/orderPage.js
+++ frontend/js/orderPage.js
@@ -1,3 +1,47 @@
+$(document).ready(function() {
+    $('#foodItem').select2();
+    applyCustomStyles();
+    
+    $('#orderForm').on('change', 'select', function() {
+        $(this).select2();
+        applyCustomStyles();
+    });
+});
+
+function applyCustomStyles() {
+    $('.select2-container--default .select2-selection--single').css({
+        'border-radius': '5px',
+        'border': '2px solid #6a0dad',
+        'padding': '10px',
+        'min-height': 'calc(2.25rem + 12px)',
+        'display': 'flex',
+        'align-items': 'center',
+        'box-sizing': 'border-box',
+    });
+
+    $('.select2-container--default .select2-selection--single .select2-selection__rendered').css({
+        'line-height': 'calc(2.25rem + 2px)',
+        'padding-top': '0',
+        'padding-bottom': '0',
+    });
+
+    $('.select2-container--default .select2-selection--single .select2-selection__arrow').css({
+        'border-left': 'none',
+        'border-radius': '0 5px 5px 0',
+        'right': '0',
+    });
+
+    $('.select2-container--default .select2-selection--single .select2-selection__placeholder').css({
+        'line-height': 'calc(2.25rem + 2px)',
+        'padding-top': '0',
+        'padding-bottom': '0',
+    });
+
+    $('.select2-container--default').css({
+        'width': '100%',
+    });
+}
+
 function addItem() {
     var container = document.getElementById("orderItems");
     var row = document.createElement("div");
@@ -23,6 +67,9 @@ function addItem() {
         </div>
     `;
     container.appendChild(row);
+
+    $(row).find('select').select2();
+    applyCustomStyles();
 }
 
 function removeItem(button) {
